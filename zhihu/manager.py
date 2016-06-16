f3e61375490402e5e18bebb4e4a7d483646fec7b
night <1179039379@qq.com>
2016-06-16 22:17:31 +0800

    change folder name

--- zhihu/manager.py
+++ zhihu/manager.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 
-from app.app import app
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
@@ -8,7 +7,6 @@ from zhihu.spiders.answer import AnswerSpider
 
 if __name__ == "__main__":
 
-    app.run()
-	# process = CrawlerProcess(get_project_settings())
-	# process.crawl(AnswerSpider)
-	# process.start() 
+	process = CrawlerProcess(get_project_settings())
+	process.crawl(AnswerSpider)
+	process.start() 
---
+++ zhihu/start_app.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from flask_app.app import app
+
+if __name__ == "__main__":
+    app.run()
