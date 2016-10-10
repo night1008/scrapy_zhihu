cf4f4056208b29705e9ef3d6574e7b9df3fb5c30
night <1179039379@qq.com>
2016-10-10 21:01:24 +0800

    add config example

--- config/scrapy_config_example.py
+++ config/scrapy_config_example.py
@@ -2,11 +2,11 @@
 
 SCRAPY = {
     'DB': {
-        'host': '127.0.0.1',
-        'port': 3306,
-        'user': 'root',
-        'passwd': '100815',
-        'db': 'zhihu',
+        'host': 'HOST',
+        'port': 'PORT',
+        'user': 'USERNAME',
+        'passwd': 'PASSWORD',
+        'db': 'DBNAME',
     },
     'BOT_NAME': 'zhihu',
     # 'SPIDER_MODULES': ['zhihu.spiders'],
